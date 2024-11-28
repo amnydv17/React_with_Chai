1649e8161a707f568882aefef5a23407ccf6f6c2
Aman Yadav <yadavaman150902@gmail.com>
2024-11-29 04:12:29 +0530

    first commit

---
+++ 03tailwindProps/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+     <h1 className='bg-orange-600 border border-blue-600 rounded-2xl p-4'>Tailwind Test</h1>
+
+     
+    </>
+  )
+}
+
+export default App
